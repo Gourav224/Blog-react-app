86d7174bc2e94aff9788ecbbfbd49370fffec15f
Gourav Kumar Dhiman <gkrdhiman@gmail.com>
2024-02-19 23:19:03 +0530

    config tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import './App.css'
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth.js";
+import { login, logout } from "./store/authSlice";
+import { Outlet } from "react-router-dom";
+import Header from "./components/header/Header.jsx";
+import Footer from "./components/footer/Footer.jsx";
 function App() {
-  return (
-    <>
-      <h1>a blog app</h1>
-    </>
-  )
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setloading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      
+      <div className="w-full block">
+        <Header />
+        <main>
+          todo
+          {" "}
+          <Outlet></Outlet>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;

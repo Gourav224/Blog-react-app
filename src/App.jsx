730f4f1ef719d57b8f59cc3dcd033f18f78bd694
Gourav Kumar Dhiman <gkrdhiman@gmail.com>
2024-02-19 21:49:00 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>a blog app</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/appwrite/auth.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-useless-catch */
+import config from "../confi/config.js"
+
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl) // Your API Endpoint
+            .setProject(config.appwriteProjectId);
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //call  another method
+                return this.login(email, password);
+            }
+            return userAccount;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite servce :: getCurrentUser :: error ", error);
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+
+}
+const authService = new AuthService();
+export default authService;
+
+
+
+
+
+
+
+
+
+
+
+
+
